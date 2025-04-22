bfd88cce5a1139111096514222b24f1f08106e05
DomiBK <148908251+DomiBK@users.noreply.github.com>
2025-04-22 23:53:41 +0200

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const images = [
+  { id: 1, alt: "Kwiat 1", filename: "kwiat1.jpg", category: 1, downloads: 35 },
+  { id: 2, alt: "Zwierzę 1", filename: "zwierze1.jpg", category: 2, downloads: 43 },
+  { id: 3, alt: "Samochód 1", filename: "samochod1.jpg", category: 3, downloads: 33 },
+  { id: 4, alt: "Kwiat 2", filename: "kwiat2.jpg", category: 1, downloads: 2 },
+  { id: 5, alt: "Zwierzę 2", filename: "zwierze2.jpg", category: 2, downloads: 53 },
+  { id: 6, alt: "Samochód 2", filename: "samochod2.jpg", category: 3, downloads: 43 }
+];
+
+export default images;
